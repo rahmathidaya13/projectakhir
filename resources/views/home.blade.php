2b4ce7f7405d4c0310e9b6f55124084758f9daa4
Sarwo_Aji_Sudirjat <sarwoajiaja@gmail.com>
2024-05-19 15:01:48 +0700

    update tampilan home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,32 +15,7 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-5 mx-auto py-5">
-                <div class="card">
-                    <h1 class="card-title text-center" >Login</h1>
-                    <div class="card-body">
-                        <form>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1"
-                                    aria-describedby="emailHelp">
-                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.
-                                </div>
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
-                            </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
-                    </div>
-                </div>
-
-            </div>
+          <h1>hello world</h1>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
